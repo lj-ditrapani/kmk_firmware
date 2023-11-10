3ed3ef8263692d1a4a2e916fde7d6b4a22b3c999
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2023-11-10 14:22:29 -0500

    feat: [sofle board] start working on ljd layout

--- boards/sofle/sofleV2/main.py
+++ boards/sofle/sofleV2/main.py
@@ -6,11 +6,10 @@ from kmk.keys import KC
 from kmk.modules.encoder import EncoderHandler
 from kmk.modules.layers import Layers
 from kmk.modules.split import Split, SplitType
+from kmk.modules.oneshot import OneShot
 
 keyboard = KMKKeyboard()
-
 layers = Layers()
-
 split = Split(
     split_flip=True,  # If both halves are the same, but flipped, set this True
     split_type=SplitType.UART,  # Defaults to UART
@@ -19,47 +18,43 @@ split = Split(
     data_pin2=board.TX,  # Second uart pin to allow 2 way communication
     use_pio=True,  # allows for UART to be used with PIO
 )
+oneshot = OneShot()
+keyboard.modules = [layers, split, oneshot]
 
-keyboard.modules = [layers, split]
-
-# Cleaner key names
-XXXXXXX = KC.NO
+xxx = KC.NO
 UNDO = KC.LCTL(KC.Z)
 CUT = KC.LCTL(KC.X)
 COPY = KC.LCTL(KC.C)
 PASTE = KC.LCTL(KC.V)
-LSTRT = KC.LCTL(KC.HOME)
-LEND = KC.LCTL(KC.END)
-BACK = KC.LALT(KC.LEFT)
-NEXT = KC.LALT(KC.RGHT)
-LBSPC = KC.LCTL(KC.BSPC)
-LOWER = KC.MO(1)
-RAISE = KC.MO(2)
+osctrl = KC.OS(KC.LSFT)
+ossft = KC.OS(KC.LSFT)
+osgui = KC.OS(KC.LSFT)
+osalt = KC.OS(KC.LSFT)
 
 keyboard.keymap = [
-    [  # QWERTY
-        # HERE----# HERE----# HERE----# HERE----# HERE----# HERE----# HERE----#ENCODER--#ENCODER--# HERE----# HERE----# HERE----# HERE----# HERE----# HERE----# HERE----
-        KC.GRV,   KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,                                            KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.GRV,
-        KC.ESC,   KC.Q,     KC.W,     KC.E,     KC.R,     KC.T,                                             KC.Y,     KC.U,     KC.I,     KC.O,     KC.P,     KC.BSPC,
-        KC.TAB,   KC.A,     KC.S,     KC.D,     KC.F,     KC.G,                                             KC.H,     KC.J,     KC.K,     KC.L,     KC.SCLN,  KC.QUOT,
-        KC.LSFT,  KC.Z,     KC.X,     KC.C,     KC.V,     KC.B,                                             KC.N,     KC.M,     KC.COMM,  KC.DOT,   KC.SLSH,  KC.RSFT,
-                            KC.LGUI,  KC.LALT,  KC.LCTL,  LOWER,    KC.ENT,   KC.MUTE,  KC.MPLY,  KC.SPC,   RAISE,    KC.RCTL,  KC.RALT,  KC.RGUI,
+    [  # Dvorak
+        # HERE----# HERE----# HERE----# HERE----# HERE----# HERE---|# HERE----# HERE----# HERE----# HERE----# HERE----# HERE----
+        xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,
+        KC.ESC,   KC.QUOT,  KC.COMM,  KC.DOT,   KC.P,     KC.Y,     KC.F,     KC.G,     KC.C,     KC.R,     KC.L,     KC.BSPC,
+        KC.LSFT,  KC.A,     KC.O,     KC.E,     KC.U,     KC.I,     KC.D,     KC.H,     KC.T,     KC.N,     KC.S,     KC.RSFT,
+        KC.TAB,   KC.SCOLON,KC.Q,     KC.J,     KC.K,     KC.X,     KC.B,     KC.M,     KC.W,     KC.V,     KC.Z,     KC.ENTER,
+        xxx,      KC.MO(7), KC.MO(3), KC.MO(1), KC.MO(5), xxx,      xxx,      KC.MO(6), KC.MO(2), KC.MO(4), KC.MO(8), xxx,
     ],
-    [  #LOWER
-        # HERE----# HERE----# HERE----# HERE----# HERE----# HERE----# HERE----#ENCODER--#ENCODER--# HERE----# HERE----# HERE----# HERE----# HERE----# HERE----# HERE----
-        XXXXXXX,  KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,                                            KC.F6,    KC.F7,    KC.F8,    KC.F9,    KC.F10,   KC.F11,
-        KC.GRV,   KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,                                            KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.F12,
-        XXXXXXX,  KC.EXLM,  KC.AT,    KC.HASH,  KC.DLR,   KC.PERC,                                          KC.CIRC,  KC.AMPR,  KC.ASTR,  KC.LPRN,  KC.RPRN,  KC.PIPE,
-        XXXXXXX,  KC.EQL,   KC.MINS,  KC.PLUS,  KC.LCBR,  KC.RCBR,                                          KC.LBRC,  KC.RBRC,  KC.SCLN,  KC.COLN,  KC.BSLS,  XXXXXXX,
-                            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    [  # alpha right
+        # HERE----# HERE----# HERE----# HERE----# HERE----# HERE---|# HERE----# HERE----# HERE----# HERE----# HERE----# HERE----
+        xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,
+        xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      KC.F,     KC.G,     KC.C,     KC.R,     KC.L,     KC.BSPC,
+        xxx,      KC.LALT,  KC.LGUI,  KC.LSFT,  KC.LCTL,  xxx,      KC.D,     KC.H,     KC.T,     KC.N,     KC.S,     KC.RSFT,
+        xxx,      osalt,    osgui,    ossft,    osctrl,   xxx,      KC.B,     KC.M,     KC.W,     KC.V,     KC.Z,     KC.ENTER,
+        xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      KC.ENTER, KC.SPACE, KC.BSPC,  xxx,      xxx,
     ],
-    [  #RAISE
-        # HERE----# HERE----# HERE----# HERE----# HERE----# HERE----# HERE----#ENCODER--#ENCODER--# HERE----# HERE----# HERE----# HERE----# HERE----# HERE----# HERE----
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                                          XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-        XXXXXXX,  KC.INS,   KC.PSCR,  KC.APP,   XXXXXXX,  XXXXXXX,                                          KC.PGUP,  BACK,     KC.UP,    NEXT,     LBSPC,    KC.BSPC,
-        XXXXXXX,  KC.LALT,  KC.LCTL,  KC.LSFT,  XXXXXXX,  KC.CAPS,                                          KC.PGDN,  KC.LEFT,  KC.DOWN,  KC.RGHT,  KC.DEL,   KC.BSPC,
-        XXXXXXX,  UNDO,     CUT,      COPY,     PASTE,    XXXXXXX,                                          XXXXXXX,  LSTRT,    XXXXXXX,  LEND,     XXXXXXX,  XXXXXXX,
-                            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    [  # alpha left
+        # HERE----# HERE----# HERE----# HERE----# HERE----# HERE---|# HERE----# HERE----# HERE----# HERE----# HERE----# HERE----
+        xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,
+        KC.ESC,   KC.QUOT,  KC.COMM,  KC.DOT,   KC.P,     KC.Y,     xxx,      xxx,      xxx,      xxx,      xxx,      xxx,
+        KC.LSFT,  KC.A,     KC.O,     KC.E,     KC.U,     KC.I,     xxx,      KC.RCTL,  KC.RSFT,  KC.RGUI,  KC.RALT,  xxx,
+        KC.TAB,   KC.SCOLON,KC.Q,     KC.J,     KC.K,     KC.X,     xxx,      osctrl,   ossft,    osgui,    osalt,    xxx,
+        xxx,      xxx,      KC.ESC,   KC.SPACE, KC.TAB,   xxx,      xxx,      xxx,      xxx,      xxx,      xxx,      xxx,
     ]
 ]
 
